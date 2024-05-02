0361c5c72f8d3c9098be5cf73d1bfd69443d73d5
Ryan Hunter <ryanhunter237@gmail.com>
2024-05-02 11:37:41 +0300

    Add test for adding two files with same groupid

--- tests/test_file_api.py
+++ tests/test_file_api.py
@@ -25,7 +25,7 @@ def runner(app: Flask) -> FlaskCliRunner:
     return app.test_cli_runner()
 
 
-def test_post_and_get_methods(client: FlaskClient):
+def test_single_post_and_get(client: FlaskClient):
     # Test POST method
     post_data = {
         "groupid": "testgroup",
@@ -46,3 +46,24 @@ def test_post_and_get_methods(client: FlaskClient):
     assert len(data) == 1
     for key in post_data:
         assert data[0][key] == post_data[key]
+
+
+def test_multiple_post_and_get(client: FlaskClient):
+    post_data = [
+        {"groupid": "gid1", "filepath": "test/path", "md5": "md51"},
+        {"groupid": "gid2", "filepath": "test/path/two", "md5": "md52"},
+        {"groupid": "gid1", "filepath": "test/path/three", "md5": "md52"},
+    ]
+    for pd in post_data:
+        response = client.post("/api/file", json=pd)
+        assert response.status_code == 201
+        assert response.get_json()["message"] == "File added successfully"
+
+    response = client.get("/api/files/gid1")
+    data = response.get_json()
+    assert response.status_code == 200
+    assert len(data) == 2
+    filepaths = [d["filepath"] for d in data]
+    assert sorted(filepaths) == ["test/path", "test/path/three"]
+    md5s = [d["md5"] for d in data]
+    assert sorted(md5s) == ["md51", "md52"]
