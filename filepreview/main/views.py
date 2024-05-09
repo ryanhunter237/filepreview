e415ce098a3a33ef279a42ede3789fe00678af54
Ryan Hunter <ryanhunter237@gmail.com>
2024-05-09 14:46:13 +0300

    Create group_id page

--- filepreview/main/views.py
+++ filepreview/main/views.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import PurePosixPath
 
 from flask import Blueprint, render_template
 
@@ -102,7 +103,32 @@ def group_page(group_id):
         .filter(File.group_id == group_id)
         .all()
     )
-    return render_template("group.html", group_id=group_id)
+
+    processed_data = {}
+    for file_path, num_bytes, thumb_path, thumb_order in data:
+        if file_path not in processed_data:
+            processed_data[file_path] = {
+                "num_bytes": num_bytes,
+                "thumbnails": [],
+            }
+        if thumb_order is not None:
+            processed_data[file_path]["thumbnails"].append((thumb_order, thumb_path))
+
+    files = []
+    for file_path in sorted(processed_data):
+        value = processed_data[file_path]
+        value["thumbnails"].sort()
+        # assuming Posix path
+        posix_path = PurePosixPath(file_path)
+        file = {
+            "directory": str(posix_path.parent),
+            "filename": posix_path.name,
+            "file_size": convert_size(value["num_bytes"]),
+            "thumbnails": [path for _, path in value["thumbnails"]],
+        }
+        files.append(file)
+
+    return render_template("group.html", group_id=group_id, files=files)
 
 
 @view_blueprint.route("/file/<path:filename>")
