48920d6ad82425b4f7b839905276640ca785ea5a
Ryan Hunter <ryanhunter237@gmail.com>
2024-05-14 11:09:05 +0300

    Change the image indicator to dots

--- filepreview/static/viewer.js
+++ filepreview/static/viewer.js
@@ -1,8 +1,7 @@
-// Using Ajax to get image urls because they can't be stored as JSON data
-// in the viewer html element
 document.addEventListener("DOMContentLoaded", function () {
   const viewer = document.getElementById("viewer");
   const imageElement = document.getElementById("currentImage");
+  const imageIndicator = document.getElementById("image-indicator");
   let images = [];
   let currentIndex = 0;
 
@@ -14,17 +13,36 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((data) => {
         images = data;
         if (images.length > 0) {
+          createImageIndicators(images.length);
           updateImage(0);
         }
       })
       .catch((error) => console.error("Error fetching images:", error));
   }
 
+  function createImageIndicators(count) {
+    imageIndicator.innerHTML = "";
+    for (let i = 0; i < count; i++) {
+      const dot = document.createElement("div");
+      dot.classList.add("dot");
+      if (i === currentIndex) {
+        dot.classList.add("active");
+      }
+      imageIndicator.appendChild(dot);
+    }
+  }
+
   function updateImage(index) {
     imageElement.src = images[index];
-    document.getElementById("imageInfo").textContent = `Image ${index + 1} of ${
-      images.length
-    }`;
+    currentIndex = index;
+    updateImageIndicators();
+  }
+
+  function updateImageIndicators() {
+    const dots = imageIndicator.querySelectorAll(".dot");
+    dots.forEach((dot, index) => {
+      dot.classList.toggle("active", index === currentIndex);
+    });
   }
 
   document.getElementById("prev").addEventListener("click", () => {
