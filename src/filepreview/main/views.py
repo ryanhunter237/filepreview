b9fef435f105892f00b90a8e23b934ae3c2f5476
Ryan Hunter <ryanhunter237@gmail.com>
2024-05-23 11:50:53 +0300

    Refactor views code

--- src/filepreview/main/views.py
+++ src/filepreview/main/views.py
@@ -11,7 +11,6 @@ from flask import (
 
 from .models import db, File, FileData, Thumbnail
 
-
 view_blueprint = Blueprint("view", __name__)
 
 
@@ -23,6 +22,43 @@ def convert_size(size: int):
         size /= 1024
         suffix_idx += 1
     return f"{round(size)} {suffixes[suffix_idx]}"
+
+
+def get_data_query():
+    return (
+        db.session.query(
+            File.group_id,
+            File.directory,
+            File.filename,
+            FileData.num_bytes,
+            Thumbnail.path,
+            Thumbnail.order,
+        )
+        .outerjoin(FileData, File.md5 == FileData.md5)
+        .outerjoin(Thumbnail, File.md5 == Thumbnail.md5)
+    )
+
+
+def process_data(data, include_group_id=True):
+    processed_data = {}
+    for record in data:
+        if include_group_id:
+            group_id, directory, filename, num_bytes, thumb_path, thumb_order = record
+            key = (group_id, directory, filename)
+        else:
+            directory, filename, num_bytes, thumb_path, thumb_order = record[1:]
+            key = (directory, filename)
+
+        if key not in processed_data:
+            processed_data[key] = {
+                "directory": directory,
+                "filename": filename,
+                "num_bytes": num_bytes,
+                "thumbnails": [],
+            }
+        if thumb_order is not None:
+            processed_data[key]["thumbnails"].append((thumb_order, thumb_path))
+    return processed_data
 
 
 @view_blueprint.route("/thumbnail/<path:filepath>")
@@ -40,65 +76,15 @@ def index() -> str:
     filename_filter = request.args.get("filename", "").strip()
     extension_filter = request.args.get("extension", "").strip()
 
-    data_query = (
-        db.session.query(
-            File.group_id,
-            File.directory,
-            File.filename,
-            FileData.num_bytes,
-            Thumbnail.path,
-            Thumbnail.order,
-        )
-        .outerjoin(FileData, File.md5 == FileData.md5)
-        .outerjoin(Thumbnail, File.md5 == Thumbnail.md5)
-    )
-
+    data_query = get_data_query()
     if filename_filter:
         data_query = data_query.filter(File.filename.ilike(f"%{filename_filter}%"))
     if extension_filter:
         data_query = data_query.filter(File.filename.ilike(f"%{extension_filter}"))
+
     data = data_query.all()
-
-    # processed_data of the form
-    # (group_id, directory, filename) -> (directory, filename, num_bytes, thumbnails)
-    processed_data = {}
-    for group_id, directory, filename, num_bytes, thumb_path, thumb_order in data:
-        key = (group_id, directory, filename)
-        if key not in processed_data:
-            processed_data[key] = {
-                "directory": directory,
-                "filename": filename,
-                "num_bytes": num_bytes,
-                "thumbnails": [],
-            }
-        if thumb_order is not None:
-            processed_data[key]["thumbnails"].append((thumb_order, thumb_path))
-
-    # files of the form
-    # (group_id, directory, filename, file_size, thumbnails)
-    # or if it's the first file for a group_id, then
-    # (group_id, directory, filename, file_size, thumbnails, rowspan)
-    files = []
-    unique_group_ids = sorted(set(group_id for group_id, *_ in processed_data))
-    for group_id in unique_group_ids:
-        data_for_group_id = [
-            value for key, value in processed_data.items() if key[0] == group_id
-        ]
-        data_for_group_id.sort(key=lambda x: x["filename"])
-        for i, value in enumerate(data_for_group_id):
-            # Sorts by thumbnail order
-            value["thumbnails"].sort()
-            file = {
-                "group_id": group_id,
-                "directory": value["directory"],
-                "filename": value["filename"],
-                "file_size": convert_size(value["num_bytes"]),
-                "thumbnails": [path for _, path in value["thumbnails"]],
-            }
-            # the first file for every group_id gets the rowspan
-            if i == 0:
-                file["rowspan"] = len(data_for_group_id)
-            files.append(file)
+    processed_data = process_data(data, include_group_id=True)
+    files = organize_files_for_index(processed_data)
 
     return render_template(
         "index.html",
@@ -108,34 +94,40 @@ def index() -> str:
     )
 
 
+def organize_files_for_index(processed_data):
+    files = []
+    unique_group_ids = sorted(set(group_id for group_id, *_ in processed_data))
+    for group_id in unique_group_ids:
+        data_for_group_id = [
+            value for key, value in processed_data.items() if key[0] == group_id
+        ]
+        data_for_group_id.sort(key=lambda x: x["filename"])
+        for i, value in enumerate(data_for_group_id):
+            value["thumbnails"].sort()
+            file = {
+                "group_id": group_id,
+                "directory": value["directory"],
+                "filename": value["filename"],
+                "file_size": convert_size(value["num_bytes"]),
+                "thumbnails": [path for _, path in value["thumbnails"]],
+            }
+            if i == 0:
+                file["rowspan"] = len(data_for_group_id)
+            files.append(file)
+    return files
+
+
 @view_blueprint.route("/group", methods=["GET"])
 def group_page():
     group_id = request.args.get("group_id", "").strip()
-    data = (
-        db.session.query(
-            File.directory,
-            File.filename,
-            FileData.num_bytes,
-            Thumbnail.path,
-            Thumbnail.order,
-        )
-        .outerjoin(FileData, File.md5 == FileData.md5)
-        .outerjoin(Thumbnail, File.md5 == Thumbnail.md5)
-        .filter(File.group_id == group_id)
-        .all()
-    )
+    data = get_data_query().filter(File.group_id == group_id).all()
+    processed_data = process_data(data, include_group_id=False)
+    files = organize_files_for_group(processed_data)
 
-    processed_data = {}
-    for directory, filename, num_bytes, thumb_path, thumb_order in data:
-        key = (directory, filename)
-        if key not in processed_data:
-            processed_data[key] = {
-                "num_bytes": num_bytes,
-                "thumbnails": [],
-            }
-        if thumb_order is not None:
-            processed_data[key]["thumbnails"].append((thumb_order, thumb_path))
+    return render_template("group.html", group_id=group_id, files=files)
 
+
+def organize_files_for_group(processed_data):
     files = []
     for key in sorted(processed_data):
         value = processed_data[key]
@@ -148,8 +140,7 @@ def group_page():
             "thumbnails": [path for _, path in value["thumbnails"]],
         }
         files.append(file)
-
-    return render_template("group.html", group_id=group_id, files=files)
+    return files
 
 
 @view_blueprint.route("/file")
@@ -159,9 +150,7 @@ def file_page():
     filename = request.args.get("filename").strip()
 
     result = (
-        db.session.query(
-            File.md5,
-        )
+        db.session.query(File.md5)
         .filter(File.group_id == group_id)
         .filter(File.directory == directory)
         .filter(File.filename == filename)
